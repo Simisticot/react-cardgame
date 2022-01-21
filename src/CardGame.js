e66a790f8251f8b2b70bb5a4cb2b4dee44cda332
Simisticot <32070013+Simisticot@users.noreply.github.com>
2022-01-21 16:11:44 +0100

    Draw

--- src/CardGame.js
+++ src/CardGame.js
@@ -1,20 +1,89 @@
 import React from "react";
 class CardGame extends React.Component {
+    
     constructor(props){
         super(props);
         this.state = {
-            DrawCooldown:0
+            DrawCooldown:0,
+            DrawnCard:{
+                name:"",
+                royalRoad:0
+            }
         };
+        this.drawcd = this.drawcd.bind(this);
+        this.updateDrawCD = this.updateDrawCD.bind(this);
+        this.draw = this.draw.bind(this);
+
+        this.drawInterval = 0;
+        this.cards = [
+            {
+                name:"",
+                royalRoad:0
+            },
+            {
+                name:"balance",
+                royalRoad:1
+            },
+            {
+                name:"bole",
+                royalRoad:1
+            },
+            {
+                name:"spire",
+                royalRoad:2
+            },
+            {
+                name:"ewer",
+                royalRoad:2
+            },
+            {
+                name:"spear",
+                royalRoad:3
+            },
+            {
+                name:"arrow",
+                royalRoad:3
+            }
+        ]
     }
+
+    draw(){
+        if(this.drawInterval != 0)return;
+        this.drawcd();
+        let rand = this.randomCard();
+        this.setState({DrawnCard:this.cards[rand]});
+    }
+
+    randomCard(){
+        return Math.floor(Math.random()*6)+1;
+    }
+
+    drawcd(){
+        this.setState({
+            DrawCooldown:5
+        });
+        this.drawInterval = setInterval(this.updateDrawCD, 1000);
+    }
+
+    updateDrawCD(){
+        this.setState(prevState => ({
+            DrawCooldown: prevState.DrawCooldown-1
+        }));
+        if(this.state.DrawCooldown === 0){
+            clearInterval(this.drawInterval);
+            this.drawInterval = 0;
+        }
+    }
+
     render(){
         return (
             <div id="cardGame">
             <div id="drawSection">
-                <button id="draw">
+                <button id="draw" onClick={this.draw}>
                 Draw 
                 </button>
-                <span id="drawCd">{ this.state.DrawCooldown }</span>
-                <span id="drawnCard"></span>
+                <span id="drawCd">{ this.state.DrawCooldown > 0 && this.state.DrawCooldown }</span>
+                <span id="drawnCard"> { this.state.DrawnCard.name } </span>
             </div>
             <div id="royalRoadSection">
                 <button id="royalRoad">
@@ -35,22 +104,6 @@ class CardGame extends React.Component {
                 <span id="sleeveDrawCD"></span>
             </div>
             <div id="party">
-                <table id="partyList">
-                    <tr>
-                        <th>Job</th>
-                        <th>Name</th>
-                        <th>DPS</th>
-                        <th>Damage</th>
-                        <th>Action</th>
-                    </tr>
-                    <tr className="player">
-                        <td>DRG</td>
-                        <td>Aquila</td>
-                        <td className="damage">0</td>
-                        <td className="dps">6000</td>
-                        <td><button className="play">Play</button></td>
-                    </tr>
-                </table>
             </div>
         </div>
         );
